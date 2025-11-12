b3d53bbcba99ce24a2ddf603c89bee789da5e1f3
natet <natet@Nates_laptop>
2025-11-11 16:37:18 -0800

    progress

--- FuncsForDatasets/LiveLongerFunc.m
+++ FuncsForDatasets/LiveLongerFunc.m
@@ -1,3 +1,12 @@
 function [lifeMatx] = LiveLongerFunc(age)
 
-data = readtable("DataSets\LiveLongerData.csv", range="A:D",VariableNamingRule="preserve"); %pull data
+DATA = readtable("DataSets\LiveLongerData.csv", range="A:D",VariableNamingRule="preserve"); %pull data
+
+Q1 = 'How many days a month do you smoke? ';
+clc;
+Q2 = 'On average, how many hours per day do you spend sitting down? ';
+clc;
+
+
+
+end
